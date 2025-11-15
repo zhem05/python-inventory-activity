751af5388135a8c412ce435f630ae93e6a2011fc
zhem05 <160566952+zhem05@users.noreply.github.com>
2025-11-15 22:00:46 +0800

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,66 @@
+def Inventory():
+    items = []
+    item_prices = {}
+
+    print("===== INVENTORY MENU =====")
+    print("[1] Add Item")
+    print("[2] Update Price")
+    print("[3] Exit")
+
+    while True:
+        try:
+            choice = int(input("Choice: "))
+
+            if choice == 1:
+                item_name = input("Enter item name: ")
+
+                if not item_name.strip():
+                    print("Item name cannot be empty.")
+                    continue
+                
+                if item_name in items:
+                    print(f"Item {item_name} already exists in inventory.")
+                    continue
+
+                try:
+                    price = float(input("Enter price: "))
+
+                    if price < 0:
+                        print("Price cannot be negative.")
+                        continue
+                    items.append(item_name)
+                    item_prices[item_name] = price
+
+                    print("Item added successfully!")
+                except ValueError:
+                    print("Invalid price. Please enter a numeric value.")
+                    continue
+            
+            elif choice == 2:
+                item_name = input("Enter item name to update: ")
+
+                if item_name not in items:
+                    print("Item not found in the inventory.")
+                    continue
+
+                try:
+                    new_price = float(input("Enter new price: "))
+
+                    if new_price < 0:
+                        print("Price cannot be negative.")
+                        continue
+
+                    item_prices[item_name] = new_price
+                    print("Price updated successfully!")
+                except ValueError:
+                    print("Invalid price. Please enter a numeric value.")
+                    continue
+            elif choice == 3:
+                print("Exiting system...")
+                break
+        except Exception as e:
+            print(f"An error occurred: {e}. Please try again")
+                    
+
+if __name__ == "__main__":
+    Inventory()
